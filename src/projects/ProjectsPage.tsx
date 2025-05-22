12de8cedf65e4d7d50becac7d4772e0b3b6e9f95
ricardo <ricardo.dev295@gmail.com>
2025-05-22 09:06:49 -0600

    Lab 5: Data - Completed

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,5 +1,13 @@
+
+import { MOCK_PROJECTS } from "./MockProjects";
+
 function ProjetsPage(){
-    return <h1>Projects</h1>
+  return (
+   <>
+     <h1>Projects</h1>
+    <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+   </>
+  );
 }
 
 export default ProjetsPage;

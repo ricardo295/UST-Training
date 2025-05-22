dce0d0201aa29dee296198a80d522341f91d92ee
ricardo <ricardo.dev295@gmail.com>
2025-05-22 09:16:30 -0600

    Lab 6: Passing data to a component - Completed

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,11 +1,12 @@
 
 import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from './ProjectList';
 
 function ProjetsPage(){
   return (
    <>
      <h1>Projects</h1>
-    <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+    <ProjectList projects={MOCK_PROJECTS} />
    </>
   );
 }

0eaf7570a56be7bd4545d55590233eb84910c049
ricardo <ricardo.dev295@gmail.com>
2025-05-23 12:02:08 -0600

    Lab 12: Hiding and showing components - Completed

--- src/projects/ProjectList.jsx
+++ src/projects/ProjectList.jsx
@@ -1,20 +1,24 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
 
+
 function ProjectList ({ projects }) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
   const handleEdit = (project) => {
-    console.log(project);
+    setProjectBeingEdited(project);
   };
 
     const items = projects.map(project => (
       <div key={project.id} className="cols-sm">
-        <ProjectCard 
-          project={project}
-          onEdit={handleEdit} 
-          ></ProjectCard> 
-        <ProjectForm/>
+        
+        {project === projectBeingEdited ? (
+            <ProjectForm />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
       </div>
     ));
     return <div className="row">{items}</div>;
